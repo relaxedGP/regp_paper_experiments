8e72044d65a80617312958e77586688aabce5daf
Julien Bect <julien.bect@centralesupelec.fr>
2024-01-11 12:17:03 +0100

    Make it possible to run without slurm

--- run_ego.sh
+++ run_ego.sh
@@ -15,7 +15,18 @@ echo "export SMC_METHOD=restart" >> jobscript.sh
 echo "source ./regp/bin/activate" >> jobscript.sh
 echo "python3 -u ./run/bench_optim.py" >> jobscript.sh
 
-#submit the job
-sbatch -a 0-$4 jobscript.sh
+## Submit the job
+if command -v sbatch &> /dev/null
+then
+    ## Run with the SLURM scheduler
+    sbatch -a 0-$4 jobscript.sh
+else
+    ## Run locally
+    for a in $(seq 0 1 $4)
+    do
+	export SLURM_ARRAY_TASK_ID=$a
+	bash jobscript.sh
+    done
+fi
 
 rm jobscript.sh
