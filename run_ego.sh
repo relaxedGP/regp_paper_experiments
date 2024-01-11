c5396f64cf5e4ce8d05a72b0891b6f57db7c6900
Julien Bect <julien.bect@centralesupelec.fr>
2024-01-11 15:02:47 +0100

    Improve launcher again (check & display input args)

--- run_ego.sh
+++ run_ego.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
+set -e
 
-mkdir -p $1/report
+## $1 = output directory
+if [ -z "$1" ]; then
+    echo Argument \$1 missing: output directory
+    exit 1
+fi
+OUTPUT_DIR=$1
+echo OUTPUT_DIR=$OUTPUT_DIR
 
-## Number of runs
+## $2 = problem name
+if [ -z "$2" ]; then
+    echo Argument \$2 missing: problem name
+    exit 1
+fi
+PROBLEM=$2
+echo PROBLEM=$PROBLEM
+
+## $3 = strategy
+if [ -z "$3" ]; then
+    echo Argument \$3 missing: strategy
+    exit 1
+fi
+STRATEGY=$3
+echo STRATEGY=$STRATEGY
+
+## $4 = number of runs
+if [ -z "$4" ]; then
+    echo Argument \$4 missing: number of runs
+    exit 1
+fi
 NB_RUNS=$4
 echo NB_RUNS=$NB_RUNS
 
@@ -10,15 +37,21 @@ echo NB_RUNS=$NB_RUNS
 TASK_ID_MIN=0
 TASK_ID_MAX=$((NB_RUNS - 1))
 
+## SMC flavour
+SMC_METHOD=restart
+echo SMC_METHOD=$SMC_METHOD
+
+mkdir -p $OUTPUT_DIR/report
+
 echo "#!/bin/bash" > jobscript.sh
 echo "#SBATCH --job-name=regp_bench" >> jobscript.sh
-echo "#SBATCH -o $1/report/output.%a.out" >> jobscript.sh
-echo "#SBATCH -e $1/report/error.%a.err" >> jobscript.sh
+echo "#SBATCH -o $OUTPUT_DIR/report/output.%a.out" >> jobscript.sh
+echo "#SBATCH -e $OUTPUT_DIR/report/error.%a.err" >> jobscript.sh
 
-echo "export OUTPUT_DIR=$1" >> jobscript.sh
-echo "export PROBLEM=$2" >> jobscript.sh
-echo "export STRATEGY=$3" >> jobscript.sh
-echo "export SMC_METHOD=restart" >> jobscript.sh
+echo "export OUTPUT_DIR=$OUTPUT_DIR" >> jobscript.sh
+echo "export PROBLEM=$PROBLEM" >> jobscript.sh
+echo "export STRATEGY=$STRATEGY" >> jobscript.sh
+echo "export SMC_METHOD=$SMC_METHOD" >> jobscript.sh
 
 if [ -n "$MODULE_LOAD" ]; then
     echo "module load $MODULE_LOAD" >> jobscript.sh
