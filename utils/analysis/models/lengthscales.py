62f610839c67f67379f22390d6736f9e2d345ee9
Sébastien J. Petit <sebastien.petit@lne.fr>
2024-06-20 14:26:36 +0200

    param analysis

---
+++ utils/analysis/models/lengthscales.py
@@ -0,0 +1,40 @@
+import numpy as np
+import sys, os
+import matplotlib.pyplot as plt
+import gpmpcontrib.levelset.test_problems as test_problems
+
+input_folder = sys.argv[1]
+test_function = sys.argv[2]
+idx_covparam = int(sys.argv[3])
+
+seq_method = "straddle"
+
+regp_methods_palette = {"Concentration": "g", "Constant": "b", "Spatial": "k", "None": "r"}
+
+n_runs = 30
+
+problem = getattr(test_problems, test_function)
+
+d = problem.input_box[1][idx_covparam] - problem.input_box[0][idx_covparam]
+
+plt.figure()
+
+for regp_method in ["None", "Constant", "Concentration", "Spatial"]:
+    path = os.path.join(input_folder, seq_method, test_function, regp_method)
+
+    full_data = []
+
+    for i in range(n_runs):
+        data = np.exp(- np.load(os.path.join(path, "covparam_{}.npy".format(i))))
+        data_i = data[:, idx_covparam + 1]/d
+
+        full_data.append(data_i)
+
+    full_data = np.array(full_data)
+    lower, med, upper = np.quantile(full_data, 0.1, axis=0), np.quantile(full_data, 0.5, axis=0), np.quantile(full_data, 0.9, axis=0)
+
+    plt.fill_between(range(med.shape[0]), lower, upper, alpha=0.2, color=regp_methods_palette[regp_method])
+    plt.plot(med, linestyle="solid", color=regp_methods_palette[regp_method])
+
+plt.semilogy()
+plt.show()
---
+++ utils/analysis/models/mean.py
@@ -0,0 +1,36 @@
+import numpy as np
+import sys, os
+import matplotlib.pyplot as plt
+import gpmpcontrib.levelset.test_problems as test_problems
+
+input_folder = sys.argv[1]
+test_function = sys.argv[2]
+
+seq_method = "straddle"
+
+regp_methods_palette = {"Concentration": "g", "Constant": "b", "Spatial": "k", "None": "r"}
+
+n_runs = 30
+
+problem = getattr(test_problems, test_function)
+
+plt.figure()
+
+for regp_method in ["None", "Constant", "Concentration", "Spatial"]:
+    path = os.path.join(input_folder, seq_method, test_function, regp_method)
+
+    full_data = []
+
+    for i in range(n_runs):
+        data = np.load(os.path.join(path, "meanparam_{}.npy".format(i)))
+        data_i = data.ravel()
+
+        full_data.append(data_i)
+
+    full_data = np.array(full_data)
+    lower, med, upper = np.quantile(full_data, 0.1, axis=0), np.quantile(full_data, 0.5, axis=0), np.quantile(full_data, 0.9, axis=0)
+
+    plt.fill_between(range(med.shape[0]), lower, upper, alpha=0.2, color=regp_methods_palette[regp_method])
+    plt.plot(med, linestyle="solid", color=regp_methods_palette[regp_method])
+
+plt.show()
