2b4c974750d15ded522ebea7ea281b4be35a69b9
Sébastien Petit <sebastien.petit@lne.fr>
2025-07-29 16:22:40 +0200

    Spatial only for noise

--- run_allnoisymethods.sh
+++ run_allnoisymethods.sh
@@ -8,7 +8,7 @@ SEQUENTIAL_STRATEGY=$3
 RUN_EGO="bash run_method.sh"
 RESULTS="results"
 
-for METHOD in None-Noisy Constant-Noisy Concentration-Noisy Spatial-Noisy
+for METHOD in None-Noisy Spatial-Noisy
 do
   $RUN_EGO $RESULTS $CASE $METHOD $REP $SEQUENTIAL_STRATEGY
 done
